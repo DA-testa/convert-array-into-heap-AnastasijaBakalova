53e61faf90ce316f343ea26d5458e4a7d846942a
Bakalova Anastasija <125196954+AnastasijaBakalova@users.noreply.github.com>
2023-03-06 19:24:47 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,36 @@
 # python3
-
+def heap(data, nodes, i, swaps):
+  s=i
+  if (2*i+1<nodes) and data[2*i+1]<data[s]:
+    s=2*i+1
+  if (2*i+2<nodes) and data[2*i+2]<data[s]:
+    s=2*i+1
+  if i!=s:
+    swaps.append((i,s))
+    return s
+  else:
+    return i
 
 def build_heap(data):
-    swaps = []
+  swaps=[]
+  
+  nodes=len(data)
+
+  for i in range(nodes//2, -1, -1 ):
+    while True:
+      s = heap(data,nodes,i, swaps)
+      if s!=i:
+        i=s
+      else:
+        i=s
+        break
+    
+  #print(data)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
 
-    return swaps
+  return swaps
 
 
 def main():
@@ -15,12 +38,22 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
+  bool1 = False
+  readfrom = input()
+  if ("I" in readfrom) or ("i" in readfrom):
+    bool1 =True
     n = int(input())
     data = list(map(int, input().split()))
-
+    
+  if ("F" in readfrom) or ("f" in readfrom):
+    name = "test/"+input()
+    if not("a" in name):
+      bool1 =True
+      with open(name) as file:
+        n = int(next(file))
+        for line in file:
+            data= list ([int(j) for j in line.split()])
+  if bool1:  
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
